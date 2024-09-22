4dec69f7a1580b1ad57c823d3cc12df63741c849
blacksnowmartin <martinmwendwa005@gmail.com>
2024-09-22 15:51:37 +0300

    i dont know

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,21 @@ import { redirect } from "next/navigation";
 
 export default function Home() {
   const user = useUser();
+
+  // Early return if user is logged in
   if (user) {
-    redirect("/home");
+    return redirect("/home");
   }
 
   return (
     <div className="max-w-screen flex h-screen max-h-screen w-screen flex-col">
-      <div className="border-gray-20 flex items-center border-b bg-primary-foreground px-6 py-4">
+      <header className="border-gray-20 flex items-center border-b bg-primary-foreground px-6 py-4">
         <h1 className="text-xl font-semibold">Web3 App</h1>
         <div className="flex-grow" />
         <div className="mr-4">
           <ModeToggle />
         </div>
-      </div>
+      </header>
       <main className="flex max-h-screen grow flex-col items-center justify-center gap-4 sm:p-8 md:p-4 lg:p-6 xl:p-8 2xl:p-24">
         <LoginForm />
       </main>
